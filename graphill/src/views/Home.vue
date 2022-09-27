3bdee13d3cc5211f2b28a7511531621e376d0a31
Joel <joelgabbai@gmail.com>
2022-09-27 15:55:27 +0200

    First version of header and footer

--- graphill/src/views/Home.vue
+++ graphill/src/views/Home.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="text-center">
-    This is the Homepage
-  </div>
+  <BaseHeader/>
+    <div class="min-h-screen bg-bodyBlue">
+      <slot />
+    </div>
+  <BaseFooter/>
 </template>
 
 <script>
+import BaseHeader from '@/components/global/BaseHeader.vue'
+import BaseFooter from '@/components/global/BaseFooter.vue'
 export default {
   name: 'HomeView',
+  components: {
+    BaseHeader,
+    BaseFooter,
+  },
 }
 </script>
