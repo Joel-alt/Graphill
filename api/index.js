0b1ae43df1b28df6f32b63e0157020de3b2409b1
bastien707 <bastien.facqueur@efrei.net>
2022-10-19 16:11:44 +0200

    [NEW] set up pool

--- api/index.js
+++ api/index.js
@@ -1,21 +1,5 @@
 var express = require('express');
 var app = express();
-const mysql = require('mysql2');
-const dotenv = require('dotenv');
-dotenv.config();
-
-const connection = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.DBUSER,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.PORT
-});
-
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
 
 app.get('/', function(req, res){
    res.send("Hello world!");
