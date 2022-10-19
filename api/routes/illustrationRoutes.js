f378fb7ad01d5b9b606c18e46d3ea8bd72424b79
Joel <joelgabbai@gmail.com>
2022-10-19 20:14:43 +0200

    [ADD] authentification file and page securisation

--- api/routes/illustrationRoutes.js
+++ api/routes/illustrationRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const illustrationController = require("../controller/illustrationController");
+const auth = require('../middleware/auth');
 
-router.get('/all', illustrationController.allIllustrations);
-router.get('/:id', illustrationController.illustrationById);
+
+router.get('/all', auth, illustrationController.allIllustrations);
+router.get('/:id', auth,illustrationController.illustrationById);
 
 module.exports = router;
