86570c24003ea5a36cc53aa6a207256dfbc2f8f7
bastien707 <bastien.facqueur@efrei.net>
2022-10-19 23:18:07 +0200

    [UPDATE] remove fixed footer

--- client/src/components/global/BaseFooter.vue
+++ client/src/components/global/BaseFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-gradient-to-t from-footerBlue to-bodyBlue fixed bottom-0 w-full">
+  <footer class="bg-gradient-to-t from-footerBlue to-bodyBlue bottom-0 w-full">
     <p class="pb-6 pt-12 text-center">© 2022 Graphill. Tous droits réservés.</p>
   </footer>
 </template>
