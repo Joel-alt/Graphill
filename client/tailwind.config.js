c046d90c9c875192a3f166b9f023c43144225703
bastien707 <bastien.facqueur@efrei.net>
2022-10-19 23:47:36 +0200

    [UDPATE] add colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
             'headerBlue': '#709CA7',
             'bodyBlue': '#ECFBFF',
             'footerBlue': '#344D59',
+            'white': '#FFFFFF',
+            'black': '#000000',
+            'red-400': 'rgb(248 113 113)',
+            'red-500': 'rgb(239 68 68)',
+            'red-600': 'rgb(220 38 38)',
+            'red-700':	'rgb(185 28 28)',
         }
     },
     plugins: [],
