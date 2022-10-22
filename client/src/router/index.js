7202fda0ee94d0f3ec9fb43c14c11422245777e1
FENG <38035703+HuitingFENG@users.noreply.github.com>
2022-10-22 09:50:21 +0200

    Update index.js

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Illustration from '../views/Illustration.vue'
+import About from '../views/About.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/illustration',
     name: 'illustration',
     component: Illustration
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About
   }
 ]
 
