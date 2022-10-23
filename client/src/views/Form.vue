7e6b59c5f315ba015a0b5ed262dfaba3fb11b279
FENG <38035703+HuitingFENG@users.noreply.github.com>
2022-10-23 12:48:43 +0200

    Update Form.vue

--- client/src/views/Form.vue
+++ client/src/views/Form.vue
@@ -1,86 +1,59 @@
 <template>
-  <div class="flex flex-col items-center"><br />
-        <input class="bg-black text-gray-500 text-white" type="text" name="lastname" v-model="lastname" @keyup.enter="addData" placeholder=" Last Name"/> <br />
-        <input class="bg-black text-gray-500 text-white" type="text" name="firstname" v-model="firstname" @keyup.enter="addData" placeholder=" First Name"/> <br />
-        <input class="bg-black text-gray-500 text-white" type="text" name="artistname" v-model="artistname" @keyup.enter="addData" placeholder=" Artist Name"/> <br />
-        <input class="bg-black text-gray-500 text-white" type="text" name="studies" v-model="studies" @keyup.enter="addData" placeholder=" studies"/> <br />
-        <input class="bg-black text-gray-500 text-white" type="text" name="hostingTime" v-model="hostingTime" @keyup.enter="addData" placeholder=" Hosting Time"/> <br />
-        <button class="px-4 py-1 text-sm font-semibold rounded-full border border-200" v-on:click="say('Submit successfully!')"> Submit </button> 
-  </div>
-
-  <div class="flex flex-col items-center"><br />
-   <table class="table-auto border-separate border-2 border-slate-500">
-       <thead>
-           <tr>
-               <th class="border-2 border-slate-500">Last Name</th>
-               <th class="border-2 border-slate-500">First Name</th>
-               <th class="border-2 border-slate-500">Artist Name</th>
-               <th class="border-2 border-slate-500">Studies</th>
-               <th class="border-2 border-slate-500">Hosting Time</th>
-           </tr>
-       </thead>
-       <tbody>
-        <tr v-for="i in items" v-bind:key="i.id">
-          <td class="border-2 border-slate-500">{{i.lastname}}</td>
-          <td class="border-2 border-slate-500">{{i.firstname}}</td>
-          <td class="border-2 border-slate-500">{{i.artistname}}</td>
-          <td class="border-2 border-slate-500">{{i.studies}}</td>
-          <td class="border-2 border-slate-500">{{i.hostingTime}}</td>
-        </tr>
-       </tbody>
-    </table>
-    </div>
+<div class="px-10 pt-8 pb-5 mb-3 flex flex-col items-center max-w-s">
+  <form v-on:submit.prevent="onSubmit" class="bg-white shadow-md rounded px-10 pt-8 pb-5 mb-3"><br />
+        <div class="mb-6 block text-gray-700 text-base font-semibold capitalize">Veuillez remplir ce formulaire pour demander de devis :</div>
+        <input class="mb-6 shadow apprearance-none border rounded w-full py-3 px-3 text-gray-500 text-black leading-tight focus:outline-none focus:shadow-outline" type="text" name="lastname" v-model="lastname" placeholder="Nom de famille" required/> <br />
+        <input class="mb-6 shadow apprearance-none border rounded w-full py-3 px-3 text-gray-500 text-black leading-tight focus:outline-none focus:shadow-outline" type="text" name="firstname" v-model="firstname" placeholder="Prénom" required/> <br />
+        <input class="mb-6 shadow apprearance-none border rounded w-full py-3 px-3 text-gray-500 text-black leading-tight focus:outline-none focus:shadow-outline" type="text" name="artistname" v-model="artistname" placeholder="Nom d'artiste" required/> <br />
+        <input class="mb-6 shadow apprearance-none border rounded w-full py-3 px-3 text-gray-500 text-black leading-tight focus:outline-none focus:shadow-outline" type="email" name="email" v-model="email" placeholder="Email" required/> <br />
+        <input class="mb-6 shadow apprearance-none border rounded w-full py-3 px-3 text-gray-500 text-black leading-tight focus:outline-none focus:shadow-outline" type="text" name="hostingTime" v-model="hostingTime" placeholder="Durée de l'hébergement" required/> <br />
+        <button class="mb-6 bg-blue-500 hover:bg-footerBlue text-white w-full font-bold py-3 rounded focus:outline-none focus:shadow-outline" @submit.prevent="onSubmit"> Envoyer </button> 
+  </form>
+</div>
 </template>
 
 <script>
-
 export default {
   name: 'FormView',
   components: {
   },
-  async created(){
-      try {
-          const res = await axios.get(url1);
-          this.items = res.data;
-      }catch(e){
-          console.error(e);
-      }
-  },
   data(){
       return {
-          items: [],
-          lastname: "",
-          firstname: "",
-          artistname: "",
-          studies: "",
-          hostingTime: "",
-          
-      };
+          lastname: '',
+          firstname: '',
+          artistname: '',
+          email: '',
+          hostingTime: '',
+      }
   },
   methods: {
-      say: function (message){
-          alert(message)
-      },
-      addData(){
-          try{
-              fetch("http://localhost:3300/forms/add", {
-                  method: "POST",
-                  headers: {
-                      "Content-Type": "application/json"
-                  },
-                  body: JSON.stringify({
-                      lastname: this.lastname,
-                      firstname: this.firstname,
-                      artistname: this.artistname,
-                      studies: this.studies,
-                      hostingTime: this.hostingTime,
-                  })
-              })
-          }catch(e){
-              console.error(e);
-          }
+      onSubmit(){
+          fetch('http://localhost:3300/forms/all', {
+              method: "POST",
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                  lastname: this.lastname,
+                  firstname: this.firstname,
+                  artistname: this.artistname,
+                  email: this.email,
+                  hostingTime: this.hostingTime,
+              }),
+          }).then((res) => res.json())
+          .then((data) => {
+              if (data.message==="Form submited !"){
+                  router.push({path: '/all'});
+              }
+          })
+          alert("Bonjour, " + this.artistname + ". Votre demande a été prise en compte et vous aurez le devis d'ici quelques jours. Merci !")
+          this.lastname = ''
+          this.firstname = ''
+          this.artistname = ''
+          this.email = ''
+          this.hostingTime = ''
       },
   }
-  
 }
 </script>
+
