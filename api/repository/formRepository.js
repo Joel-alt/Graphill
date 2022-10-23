b9ea8b0460d587c00268e1838a2ec34014d500cd
FENG <38035703+HuitingFENG@users.noreply.github.com>
2022-10-23 12:43:25 +0200

    Update formRepository.js

--- api/repository/formRepository.js
+++ api/repository/formRepository.js
@@ -1,26 +1,12 @@
 const utility = require("../utility/index");
 
-const addForm = async (lastname, firstname, artistname, studies, hostingTime) => {
+const all = async (lastname, firstname, artistname, email, hostingTime) => {
     const pool = utility.pool;
-    const query = `INSERT INTO form ('lastname', 'firstname', 'artistname', 'studies', 'hostingTime') VALUES (${lastname},${firstname},${artistname},${studies},${hostingTime})`;
+    const query = `INSERT INTO form (lastname, firstname, artistname, email, hostingTime) VALUES ("${lastname}","${firstname}","${artistname}","${email}","${hostingTime}")`;
     const promisePool = pool.promise();
     return await promisePool.query(query);
 }
 
-const allForms = async () => {
-    const pool = utility.pool;
-    const query = `SELECT * FROM form`;
-    const promisePool = pool.promise();
-    return await promisePool.query(query);
-}
 
-const formById = async (id) => {
-    const pool = utility.pool;
-    const query = `SELECT * FROM form WHERE id = ${id}`;
-    const promisePool = pool.promise();
-    return await promisePool.query(query);
-}
+exports.all = all;
 
-exports.addForm = addForm;
-exports.allForms = allForms;
-exports.formById = formById;
