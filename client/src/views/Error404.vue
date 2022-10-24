a25cd0a0dd586b42a3b8e045963b32810c5fd8dd
bastien707 <bastien.facqueur@efrei.net>
2022-10-24 23:36:04 +0200

    [NEW] add catch all endpoints

---
+++ client/src/views/Error404.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex justify-center align-middle h-full">
+        <h1 class="font-bold text-5xl text-center">
+            Oops... <br />
+            Cette page n'existe pas ! <br />
+            <a href="/" class="text-violet-600"> Revenir au menu principal 😎</a>
+        </h1>
+    </div>
+</template>
